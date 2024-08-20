9092668a3a6dd2ee9a1a4ea08d949932e86831ae
Elo-oghene <elooghenejoy@gmail.com>
2024-08-20 12:34:36 +0100

    Initial commit

---
+++ app/NavBar.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+ 
+
+  return (
+    <div className='flex bg-slate-200 p-3 space-x-3'>
+      <Link href="/" className='mr-5'>Next.js</Link>
+      <Link href="/users">Users</Link>
+      
+    </div>
+  )
+}
+
+export default NavBar
