321f794e33738720b820a4d2d74c20d11c54a566
Elo-oghene <elooghenejoy@gmail.com>
2024-08-26 18:16:44 +0100

    Deleting objects

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -15,8 +15,17 @@ export async function PUT(
 ) {
   const body = await request.json();
   if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 404 });
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({});
 }
