9f2884952e9b63a959e905e10bf4cac49d202837
Elo-oghene <elooghenejoy@gmail.com>
2024-08-26 18:09:41 +0100

    Updating objects

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id: 1, name: "Joy" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 404 });
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id: 1, name: body.name });
+}
