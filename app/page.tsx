6f1c1df015ceee79bbe2a90500d39e2e1109c2df
Elo-oghene <elooghenejoy@gmail.com>
2024-09-07 15:41:44 +0100

    Accessing sessions on the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <button
         onClick={async () => {
           const _ = (await import("lodash")).default;
 
-          const users = [
-            { name: "c" },
-            { name: "b" },
-            { name: "a" },
-          ];
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
 
           const sorted = _.orderBy(users, ["name"]);
           console.log(sorted);
